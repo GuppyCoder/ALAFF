3c46da2851762521272152d32c3a9fc9167c7341
GuppyCoder <102766209+GuppyCoder@users.noreply.github.com>
2024-08-17 14:44:21 -0500

    Update BiRed.m
    ATL - alpha11
    ABL - a21
    ATR - a12t
    ABR - A22
    
    They seem to be correctly partitioning.

--- Assignments/Week11/matlab/BiRed.m
+++ Assignments/Week11/matlab/BiRed.m
@@ -33,16 +33,59 @@ function [ A_out, t_out, r_out ] = BiRed( A, t, r )
                                     1, 'FLA_BOTTOM' );
                                 
     %------------------------------------------------------------%
+    % Step 1: Construct the column vector x_col by combining the current 
+    % diagonal element alpha11 and the subdiagonal elements a21.
+    x_col = [alpha11; a21];
+    
+    % Step 2: Compute the Householder vector u_col and the scalar tau1
+    % using the Housev1 function. 
+    % - u_col is the Householder vector that will be used to zero out the 
+    %   elements of x_col (except for the first element).
+    % - tau1 is a scalar used in the transformation.
+    [u_col, tau1] = Housev1(x_col);
+    
+    % Store tau1 in the t vector
+    t(size(tT, 1) + 1) = tau1;
+    
+    % Step 3: Update a21 and A22 using the Householder vector
+    v = u_col(2:end);
+    alpha11 = u_col(1);  % Update the diagonal element
+    a21 = v;
+    
+    if size(A22, 1) > 1 && size(A22, 2) > 1
+        % Form the full Householder vector u_col
+        u_col_full = [1; v];
+        
+        % Apply the Householder transformation to [a12t; A22]
+        disp(size(a12t'))
+        disp(size(A22))
+        block_matrix = [a12t; A22'];
+        
+        % Householder matrix H = I - (1/tau1) * u_col_full * u_col_full'
+        H_col = eye(size(block_matrix, 1)) - (1/tau1) * (u_col_full * u_col_full');
+        
+        % Apply the transformation to the block matrix
+        transformed_block = H_col * block_matrix;
+        
+        % Extract the updated row vector a12t and submatrix A22
+        a12t = transformed_block(1, :);  % Updated row vector
+        A22 = transformed_block(2:end, :);  % Updated submatrix
+    end
 
 
     
+    % Zero out all of a21
+    a21(:) = 0;
     
     
-    
-    
-    
-    
-    
+    % Reassemble the matrix before proceeding to the row transformation
+    [ ATL, ATR, ...
+      ABL, ABR ] = FLA_Cont_with_3x3_to_2x2( A00,  a01,     A02,  ...
+                                             a10t, alpha11, a12t, ...
+                                             A20,  a21,     A22, ...
+                                             'FLA_TL' );
+
+    % Everything Until Here Looks good
     
     %------------------------------------------------------------%
 
